9281e307fb9f947612d08896e2a9264d16eb2c19
Tom-hcastle <J.E786406@gmail.com>
2024-02-12 13:45:45 +0000

    Added extra text

--- src/components/Sectiontwo.jsx
+++ src/components/Sectiontwo.jsx
@@ -13,6 +13,9 @@ export function Sectiontwo() {
             <div className="happy">Happy</div>
             <div className="valentines">Valentines</div>
             <div className="day">Day My Love!</div>
+            <div className="happy">Be</div>
+            <div className="valentines">My </div>
+            <div className="day">Valentine!</div>
             <img src={myImage} alt="Descriptive Alt Text" style={{ width: '600px', height: 'auto' }} />
         </section>
     )
